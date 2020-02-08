40b827b505255a4592220dcffbba6da8401c127f
Görög Áron <gorogaron95@gmail.com>
2020-02-08 20:01:25 +0100

    LocationListener bugfix
    potential fix for freezing screen

--- app/src/main/java/com/agrolytics/agrolytics_android/utils/AgroLocationListener.java
+++ app/src/main/java/com/agrolytics/agrolytics_android/utils/AgroLocationListener.java
@@ -28,34 +28,10 @@ public  class AgroLocationListener implements LocationListener {
 
     @Override
     public void onLocationChanged(Location loc) {
-//        Toast.makeText(
-//                context,
-//                "Location changed: Lat: " + loc.getLatitude() + " Lng: "
-//                        + loc.getLongitude(), Toast.LENGTH_SHORT).show();
-
         String longitude = "Longitude: " + loc.getLongitude();
         Log.v(TAG, longitude);
         String latitude = "Latitude: " + loc.getLatitude();
         Log.v(TAG, latitude);
-
-        /*------- To get city name from coordinates -------- */
-        String cityName = null;
-        Geocoder gcd = new Geocoder(context, Locale.getDefault());
-        List<Address> addresses;
-        try {
-            addresses = gcd.getFromLocation(loc.getLatitude(),
-                    loc.getLongitude(), 1);
-            if (addresses.size() > 0) {
-                System.out.println(addresses.get(0).getLocality());
-                cityName = addresses.get(0).getLocality();
-            }
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        String s = longitude + "\n" + latitude + "\n\nMy Current City is: "
-                + cityName;
 
         Util.Companion.setLat(loc.getLatitude());
         Util.Companion.setLong(loc.getLongitude());
